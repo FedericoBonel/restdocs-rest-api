944da423fadc4e74b047ad002cc0cae9b63585b4
Federico Jorge Bonel Tozzi <Bonelfederico@gmail.com>
2022-06-30 09:20:52 +0900

    Configured rest docs in the controller test, using AutoConfigureRestDocs is a way to avoid configuring it in the BeforeEach method.

--- src/test/java/com/federicobonel/restdocsrestapi/web/controllers/BeerControllerTest.java
+++ src/test/java/com/federicobonel/restdocsrestapi/web/controllers/BeerControllerTest.java
@@ -7,11 +7,15 @@ import com.federicobonel.restdocsrestapi.web.model.BeerDTO;
 import com.federicobonel.restdocsrestapi.web.model.BeerStyle;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
@@ -22,6 +26,8 @@ import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
 @WebMvcTest(BeerController.class)
 @ComponentScan(basePackages = "com.federicobonel.restdocsrestapi.web.mappers")
 class BeerControllerTest {
